fb8dd311d2e43e5c69d5c0e151493dcaa68e9a96
Tong Xu <erichilla0525@gmail.com>
2024-10-01 15:59:11 -0500

    Applied CSS styling for enhanced user experience.

--- script.js
+++ script.js
@@ -6,45 +6,57 @@ let currentState = 0;
 function renderQuestion(){
     const question = document.getElementById('question');
     const answers = document.getElementById('answers');
+    const image = document.getElementById('Uldua-creature');
 
     question.textContent = '';
     answers.textContent = '';
 
+    image.style.display = 'none';
+
     switch(currentState){
         case 0:
-            question.textContent = 'You meet Thorim, the Thunder Protector. Choose from these options.';
+            question.textContent = 'You enter Uldua, and meet Thorim, Thunder Protector. Choose from these options.';
+            image.src = "Uldua.png";
+            image.style.display = 'block';
             addAnswerButton('Run Away', 1);
             addAnswerButton('Fight With Thorim', 2)
             break;
 
         case 1:
-            question.textContent = 'You lost the chance to gain the power, game over.';
+            question.textContent = 'You lost the chance to gain the power, game is over.';
             addAnswerButton('Restart The Game', 0);
             break;
         case 2:
-            question.textContent = 'You defeated Thorim, the Thunder Protector. You gain his thunder power.';
+            question.textContent = 'You defeated Thorim, Thunder Protector. You gained his thunder power.';
+            image.src = "Thorim.png";
+            image.style.display = 'block';
             addAnswerButton('Keep Going', 3)
+            
             break;
 
         case 3:
-            question.textContent = 'You meet Hodir, the Ice Protector. Choose from these options.';
+            question.textContent = 'You meet Hodir, Ice Protector. Choose from these options.';
             addAnswerButton('Run Away', 1);
             addAnswerButton('Fight with Hodir', 4);
             break;
 
         case 4:
-            question.textContent = 'You defeated Hodir, the Ice Protector. You gain his ice power.';
+            question.textContent = 'You defeated Hodir, Ice Protector. You gained his ice power.';
+            image.src = "Hodir.png";
+            image.style.display = 'block';
             addAnswerButton('Keep Going', 5)
             break;
 
         case 5:
-            question.textContent = 'You meet Freya, the Life Protector. Choose from these options.';
+            question.textContent = 'You meet Freya, Life Protector. Choose from these options.';
             addAnswerButton('Run Away', 1);
             addAnswerButton('Fight with Freya', 6);
             break;
 
         case 6:
-            question.textContent = 'You defeated Freya, the Life Protector. You gain her life power.';
+            question.textContent = 'You defeated Freya, Life Protector. You gained her life power.';
+            image.src = "Freya.png";
+            image.style.display = 'block';
             addAnswerButton('Keep Going', 7);
             break;
 
@@ -56,11 +68,15 @@ function renderQuestion(){
 
         case 8:
             question.textContent = 'You defeated the Oracle: Yogg-Saron, Hope End. Uldua is saved by you.'
+            image.src = "Yogg.png";
+            image.style.display = 'block';
             addAnswerButton('Restart', 0)
             break
 
         case 9:
             question.textContent = 'You are corrupted by Yogg-Saron and been transformed as an Faceless, forever serving the Oracle: Yogg-Saron, Hope End.'
+            image.src = "Faceless.png";
+            image.style.display = 'block';
             addAnswerButton('Restart', 0)
             break
     }
